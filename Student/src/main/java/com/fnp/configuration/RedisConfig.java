0645e03bdeb2caeaacee18f962cc57d6708b3e71
saransh <saransh.v@fnp.com>
2022-12-18 18:51:46 +0530

    redis : new commit

---
+++ Student/src/main/java/com/fnp/configuration/RedisConfig.java
@@ -0,0 +1,56 @@
+package com.fnp.configuration;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fnp.dto.Student;
+
+@Configuration
+public class RedisConfig {
+	
+	@Value("${spring.redis.host}")
+    private String REDIS_HOSTNAME;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
+
+    @Bean
+    protected JedisConnectionFactory jedisConnectionFactory() {
+
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(REDIS_HOSTNAME, REDIS_PORT);
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().usePooling().build();
+        JedisConnectionFactory factory = new JedisConnectionFactory(configuration,jedisClientConfiguration);
+        factory.afterPropertiesSet();
+
+        return factory;
+
+    }
+    
+
+    @Bean
+    public RedisTemplate<String,Student> redisTemplate() {
+
+        final RedisTemplate<String,Student> redisTemplate = new RedisTemplate<String,Student>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<Student>(Student.class));
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        String status = redisTemplate.getConnectionFactory().getConnection().ping();
+        System.out.println(status);
+        return redisTemplate;
+    }
+    
+}
